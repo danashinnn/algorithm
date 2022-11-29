5c2e8a5af1e040ffb8fb88a8fafcfbb2e4152c45
danashinnn <103996283+danashinnn@users.noreply.github.com>
2022-11-29 23:08:48 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(true) {
+            answer += n%10;
+            n /= 10;
+            if(n <= 10) {
+                answer += n;
+                break;
+            }
+        }
+        return answer;
+    }
+}
