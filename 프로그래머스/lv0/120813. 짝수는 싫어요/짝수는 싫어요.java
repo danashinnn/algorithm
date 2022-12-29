360d45fb22a763c65158bc0814968f3187e8d90b
danashinnn <103996283+danashinnn@users.noreply.github.com>
2022-12-30 01:02:26 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -3,11 +3,8 @@ import java.util.*;
 class Solution {
     public ArrayList<Integer> solution(int n) {
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		list.add(1);
-		if(n > 2) {
-			for(int i=3; i<n+1; i++) {
-				if(i%2 == 1) list.add(i);
-			}
+		for(int i=1; i<n+1; i++) {
+			if(i%2 == 1) list.add(i);
 		}
 		return list;
     }
