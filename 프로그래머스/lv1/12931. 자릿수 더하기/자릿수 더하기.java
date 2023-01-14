c89076d6e09b95145990e96255c151b2851b0adb
danashinnn <103996283+danashinnn@users.noreply.github.com>
2023-01-14 17:53:17 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = String.valueOf(n);
+        for(char ch : str.toCharArray()) {
+            answer += Character.getNumericValue(ch);
+        }
+        return answer;
+    }
+}
