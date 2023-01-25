400ee590b509905a1dc474ed6a152dc03ad7a719
danashinnn <103996283+danashinnn@users.noreply.github.com>
2023-01-26 01:27:20 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        String str = new StringBuilder(Integer.toString(n, 3)).reverse().toString();
+        int answer = Integer.parseInt(str, 3);
+        return answer;
+    }
+}
